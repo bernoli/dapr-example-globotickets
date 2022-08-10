b1fd2b2b6b1043fc06d3dc96f5115a95add87277
yoav kaplan <yoav_kapl@hotmail.com>
2022-08-10 16:57:15 +0300

    adding src code

---
+++ ordering/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+using Dapr;
+using GloboTicket.Ordering.Model;
+using GloboTicket.Ordering.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.Ordering.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly ILogger<OrderController> logger;
+    private readonly EmailSender emailSender;
+
+    public OrderController(ILogger<OrderController> logger, EmailSender emailSender)
+    {
+        this.logger = logger;
+        this.emailSender = emailSender;
+        this.logger.LogInformation("Order Controller started");
+    }
+
+    [HttpPost("", Name = "SubmitOrder")]
+    [Topic("pubsub", "orders")]
+    public async Task<IActionResult> Submit(OrderForCreation order)
+    {
+        logger.LogInformation($"Received a new order from {order.CustomerDetails.Name}");
+        // await emailSender.SendEmailForOrder(order);
+        return Ok();
+    }
+}
